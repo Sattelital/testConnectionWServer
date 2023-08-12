41d253e3c671df4db594988283596698f4b9cf53
Edward Espinoza <em.esc.et@gmail.com>
2023-08-12 10:37:05 -0500

    Add js crypted & scriptReadable

--- src/app/server.js
+++ src/app/server.js
@@ -4,11 +4,7 @@ import { apiRouter } from "../routers/api.router.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
-
-app.use(express.json())
-
-
-app.use('/api', apiRouter)
-
-const httpServer = app.listen(PORT, () => { console.log(`Conectado al servidor mediante el puerto: ${PORT}`); });
+app.use(cors())
+    .use(express.json())
+    .use('/api', apiRouter)
+    .listen(PORT, () => { console.log(`Conectado al servidor mediante el puerto: ${PORT}`); });
